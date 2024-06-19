ba81d6bca3e3c3739a35bb4ad33e0cf2591ecb9c
Ominous-Joe <j.ohwonohwo@genesystechhub.com>
2024-06-19 13:38:44 -0400

    resolving conflicts

--- src/app/assets/svg/index.ts
+++ src/app/assets/svg/index.ts
@@ -1,4 +1,3 @@
-import ArrowLeftIcon from "./arrow-left.svg";
 import ChatOnWhatsappIcon from "./chat-on-whatsapp.svg";
 import ChevronRightIcon from "./chevron-right.svg";
 import DiagonalArrowIcon from "./diagonal-arrow.svg";
@@ -7,12 +6,8 @@ import OrderDeliveredIcon from "./order-delivered.svg";
 import PlaceOrderIcon from "./place-order.svg";
 import SlideLeftIcon from "./slide-left.svg";
 import SlideRightIcon from "./slide-right.svg";
-import LinkedInIcon from "./linkedIn-icon.svg";
-import InstagramIcon from "./instagram-icon.svg";
-import XIcon from "./x-icon.svg";
 
 export {
-    ArrowLeftIcon,
     ChatOnWhatsappIcon,
     ChevronRightIcon,
     DiagonalArrowIcon,
